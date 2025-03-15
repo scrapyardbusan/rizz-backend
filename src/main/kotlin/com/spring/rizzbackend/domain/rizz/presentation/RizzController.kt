db35eb0564a5b8550044df4d07dba65e88bf0fd2
최미래 <miraechoi@dgsw.hs.kr>
2025-03-15 17:59:06 +0900

    feature: add rizz controller

---
+++ src/main/kotlin/com/spring/rizzbackend/domain/rizz/presentation/RizzController.kt
@@ -0,0 +1,21 @@
+package com.spring.rizzbackend.domain.rizz.presentation
+
+import com.spring.rizzbackend.domain.rizz.application.RizzService
+import com.spring.rizzbackend.domain.rizz.application.dto.ChatGPTResponse
+import com.spring.rizzbackend.global.response.BaseResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/rizz")
+class RizzController (
+    private val rizzService: RizzService
+) {
+
+    @PostMapping
+    fun chat(@RequestBody text:String): BaseResponse<ChatGPTResponse> {
+        return rizzService.chat(text)
+    }
+}

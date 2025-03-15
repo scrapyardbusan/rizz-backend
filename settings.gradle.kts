38e37a3d0e75a34a343147d7cb9d1ead5024563c
최미래 <miraechoi@dgsw.hs.kr>
2025-03-15 17:17:33 +0900

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "rizz-backend"
---
+++ src/main/kotlin/com/spring/rizzbackend/RizzBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.rizzbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RizzBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<RizzBackendApplication>(*args)
+}

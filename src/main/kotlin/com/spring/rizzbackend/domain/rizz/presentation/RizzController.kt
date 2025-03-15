13fae274b15cbef605fbc2ecfd96fbf656e8cc43
최미래 <miraechoi@dgsw.hs.kr>
2025-03-16 03:50:19 +0900

    feature: add cors setting

--- src/main/kotlin/com/spring/rizzbackend/domain/rizz/presentation/RizzController.kt
+++ src/main/kotlin/com/spring/rizzbackend/domain/rizz/presentation/RizzController.kt
@@ -3,11 +3,9 @@ package com.spring.rizzbackend.domain.rizz.presentation
 import com.spring.rizzbackend.domain.rizz.application.RizzService
 import com.spring.rizzbackend.domain.rizz.application.dto.ChatGPTResponse
 import com.spring.rizzbackend.global.response.BaseResponse
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 @RestController
 @RequestMapping("/rizz")
 class RizzController (

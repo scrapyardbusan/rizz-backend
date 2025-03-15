4f1b0d9f069e3bbef121f19c1e343d2febd4877a
최미래 <miraechoi@dgsw.hs.kr>
2025-03-15 17:58:53 +0900

    feature: add base response

---
+++ src/main/kotlin/com/spring/rizzbackend/global/response/BaseResponse.kt
@@ -0,0 +1,15 @@
+package com.spring.rizzbackend.global.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.http.HttpStatus
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class BaseResponse<T> (
+
+    val status: Int = HttpStatus.OK.value(),
+    val success: Boolean = true,
+    val state: String? = "OK",
+    val message: String,
+    val data: T? = null
+
+)
